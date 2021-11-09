32d1adf2df98e103d72fa892e36d4983ddd15dc9
Yash Bansal <ybansalnaveen@gmail.com>
2021-11-10 01:34:39 +0530

    take_photo

--- app/src/main/java/com/applicationPantr/plantr/ui/scan/ScanActivity.kt
+++ app/src/main/java/com/applicationPantr/plantr/ui/scan/ScanActivity.kt
@@ -1,16 +1,22 @@
 package com.applicationPantr.plantr.ui.scan
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.applicationPantr.plantr.R
 import com.applicationPantr.plantr.databinding.ActivityScanBinding
+import java.io.File
 import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -18,12 +24,19 @@ class ScanActivity : AppCompatActivity() {
 
     private lateinit var activityScanBinding: ActivityScanBinding
     private lateinit var cameraExecutor: ExecutorService
+    private lateinit var outputDirectory: File
+    private lateinit var cameraProvider: ProcessCameraProvider
     private var imageCapture: ImageCapture? = null
+
+    companion object {
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activityScanBinding = DataBindingUtil.setContentView(this,R.layout.activity_scan)
+        activityScanBinding = DataBindingUtil.setContentView(this, R.layout.activity_scan)
 
+        outputDirectory = getOutputDirectory()
         cameraExecutor = Executors.newSingleThreadExecutor()
 
         startCamera()
@@ -34,21 +47,36 @@ class ScanActivity : AppCompatActivity() {
 
     }
 
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         cameraProviderFuture.addListener(Runnable {
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            cameraProvider = cameraProviderFuture.get()
 
             val preview = Preview.Builder().build().also {
                 it.setSurfaceProvider(activityScanBinding.viewFinder.surfaceProvider)
             }
 
+            imageCapture = ImageCapture.Builder().build()
+
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
             try {
                 cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(this,cameraSelector,preview)
-            }catch (e: Exception){
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+            } catch (e: Exception) {
 
             }
 
@@ -56,6 +84,35 @@ class ScanActivity : AppCompatActivity() {
     }
 
     private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+                .format(System.currentTimeMillis()) + ".jpg"
+        )
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(outputOptions,
+            ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    activityScanBinding.apply {
+                        cameraProvider.unbindAll()
+                        viewFinder.visibility = View.GONE
+                        ivScanPlant.apply {
+                            setImageURI(savedUri)
+                            visibility = View.VISIBLE
+                        }
+                        tvScanPlantName.text = "Plant Name"
+                    }
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                }
+
+            })
     }
 
     override fun onDestroy() {
