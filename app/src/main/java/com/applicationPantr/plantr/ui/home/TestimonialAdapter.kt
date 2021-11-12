a967d303ec7b90554e1b5397b336caff1656db80
Yash Bansal <ybansalnaveen@gmail.com>
2021-11-12 14:57:43 +0530

    yash_merge

--- app/src/main/java/com/applicationPantr/plantr/ui/home/TestimonialAdapter.kt
+++ app/src/main/java/com/applicationPantr/plantr/ui/home/TestimonialAdapter.kt
@@ -5,18 +5,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.applicationPantr.plantr.R
+import kotlinx.android.synthetic.main.clientreview.view.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
 import kotlinx.android.synthetic.main.item_client_reviews.view.*
+import kotlinx.android.synthetic.main.item_client_reviews.view.expert_Rating
 import kotlinx.android.synthetic.main.slider_container.view.*
 
 class TestimonialAdapter(private val TestimonialSlider: List<Testimonials>):
     RecyclerView.Adapter<TestimonialAdapter.TestViewHolder>() {
     inner class TestViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val textTittles = view.tvClientName
+        private val textTittles = view.ClientName
         private val description=view.Descr
         private val rating=view.expert_Rating
-        private val quama = view.doubleCommas
-        private val image=view.ciClientImage
+        private val quama = view.punctuation
+        private val image=view.ClientImage
         fun bind(testSlider: Testimonials) {
             textTittles.text=testSlider.names
             description.text=testSlider.HeadingTwo
@@ -29,7 +31,7 @@ class TestimonialAdapter(private val TestimonialSlider: List<Testimonials>):
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TestViewHolder {
         return TestViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_client_reviews, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.clientreview, parent, false)
         )
     }
 
